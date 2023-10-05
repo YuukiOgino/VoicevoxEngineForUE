7837a7e9c6ff7d9ce432e73101e91bb39ebbfbc3
Yuki Ogino <r1050290.uv.tuc.ac.jp@gmail.com>
2023-10-06 03:32:24 +0900

    サンプルで修正した内容をプラグインへ反映

--- Plugins/VoicevoxEngine/Source/ThirdParty/VoicevoxCore/VoicevoxCore.Build.cs
+++ Plugins/VoicevoxEngine/Source/ThirdParty/VoicevoxCore/VoicevoxCore.Build.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using UnrealBuildTool;
-using Console = Internal.Console;
 
 /// <summary>
 /// VOICEVOX COREライブラリモジュールクラス
@@ -27,72 +26,72 @@ public class VoicevoxCore : ModuleRules
 
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
+			const string platformName = "x64";
+			const string binPlatformName = "Win64";
+			
 			// core.hはEngine側も同名のヘッダーファイルがあるため、意図的にx64フォルダまでをIncludePathに含める
-			PublicSystemIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "x64")));
+			PublicSystemIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, platformName)));
 			
 			// Add the import library
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "voicevox_core.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, platformName, "voicevox_core.lib"));
 
 			// Delay-load the DLL, so we can load it from the right place first
 			PublicDelayLoadDLLs.Add("voicevox_core.dll");
 			PublicDelayLoadDLLs.Add("onnxruntime.dll");
 
 			// Ensure that the DLL is staged along with the executable
-			RuntimeDependencies.Add("$(ProjectDir)/Binaries/Win64/voicevox_core.dll", Path.Combine(ModuleDirectory, "x64", "voicevox_core.dll"));
-			RuntimeDependencies.Add("$(ProjectDir)/Binaries/Win64/onnxruntime.dll", Path.Combine(ModuleDirectory, "x64", "onnxruntime.dll"));
+			RuntimeDependencies.Add($"$(ProjectDir)/Binaries/{binPlatformName}/voicevox_core.dll", Path.Combine(ModuleDirectory, platformName, "voicevox_core.dll"));
+			RuntimeDependencies.Add($"$(ProjectDir)/Binaries/{binPlatformName}/onnxruntime.dll", Path.Combine(ModuleDirectory, platformName, "onnxruntime.dll"));
 			
-			var providersSharedPath = Path.Combine(ModuleDirectory, "x64", "onnxruntime_providers_shared.dll");
-			if (File.Exists(providersSharedPath))
+			var ProvidersSharedPath = Path.Combine(ModuleDirectory, platformName, "onnxruntime_providers_shared.dll");
+			if (File.Exists(ProvidersSharedPath))
 			{
 				PublicDelayLoadDLLs.Add("onnxruntime_providers_shared.dll");
-				RuntimeDependencies.Add("$(ProjectDir)/Binaries/Win64/onnxruntime_providers_shared.dll", providersSharedPath);
+				RuntimeDependencies.Add($"$(ProjectDir)/Binaries/{binPlatformName}/onnxruntime_providers_shared.dll", ProvidersSharedPath);
 			}
             
-			var cudaPath = Path.Combine(ModuleDirectory, "x64", "onnxruntime_providers_cuda.dll");
-			if (File.Exists(cudaPath))
+			var CudaPath = Path.Combine(ModuleDirectory, platformName, "onnxruntime_providers_cuda.dll");
+			if (File.Exists(CudaPath))
 			{
 				PublicDelayLoadDLLs.Add("onnxruntime_providers_cuda.dll");
-				RuntimeDependencies.Add("$(ProjectDir)/Binaries/Win64/onnxruntime_providers_cuda.dll", cudaPath);
+				RuntimeDependencies.Add($"$(ProjectDir)/Binaries/{binPlatformName}/onnxruntime_providers_cuda.dll", CudaPath);
 			}
 			
-			var tensorrtPath = Path.Combine(ModuleDirectory, "x64", "onnxruntime_providers_tensorrt.dll");
-			if (File.Exists(tensorrtPath))
+			var TensorrtPath = Path.Combine(ModuleDirectory, platformName, "onnxruntime_providers_tensorrt.dll");
+			if (File.Exists(TensorrtPath))
 			{
 				PublicDelayLoadDLLs.Add("onnxruntime_providers_tensorrt.dll");
-				RuntimeDependencies.Add("$(ProjectDir)/Binaries/Win64/onnxruntime_providers_tensorrt.dll", tensorrtPath);
+				RuntimeDependencies.Add($"$(ProjectDir)/Binaries/{binPlatformName}/onnxruntime_providers_tensorrt.dll", TensorrtPath);
 			}
 			
 			// Open JTalkライブラリフォルダもコピーする
-			DeleteDirectory($"{ProjectDirectory}/Binaries/Win64/{OpenJtalkDicName}");
-			DirectoryCopy(Path.Combine(ModuleDirectory, "x64", OpenJtalkDicName), $"{ProjectDirectory}/Binaries/Win64/{OpenJtalkDicName}", true);
-			
+			AddRuntimeDependenciesDirectory(OpenJtalkDicName, platformName, binPlatformName, true);
 			// modelフォルダもコピーする
-			DeleteDirectory($"{ProjectDirectory}/Binaries/Win64/model");
-			DirectoryCopy(Path.Combine(ModuleDirectory, "x64", "model"), $"{ProjectDirectory}/Binaries/Win64/model", true);
+			AddRuntimeDependenciesDirectory("model", platformName, binPlatformName, true);
         }
 		else if (Target.Platform == UnrealTargetPlatform.Mac)
 		{
+			const string platformName = "osx";
+			const string binPlatformName = "Mac";
+			
 			// core.hはEngine側も同名のヘッダーファイルがあるため、意図的にx64フォルダまでをIncludePathに含める
-			PublicSystemIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "osx")));
+			PublicSystemIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, platformName)));
 			
 			// Delay-load the DLL, so we can load it from the right place first
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "osx", "libvoicevox_core.dylib"));
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "osx", "libonnxruntime.1.13.1.dylib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, platformName, "libvoicevox_core.dylib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, platformName, "libonnxruntime.1.13.1.dylib"));
 			
-			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "osx", "libvoicevox_core.dylib"));
-			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "osx", "libonnxruntime.1.13.1.dylib"));
+			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, platformName, "libvoicevox_core.dylib"));
+			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, platformName, "libonnxruntime.1.13.1.dylib"));
 			
 			// Ensure that the DLL is staged along with the executable
-			RuntimeDependencies.Add("$(ProjectDir)/Binaries/Mac/libvoicevox_core.dylib", Path.Combine(ModuleDirectory, "osx", "libvoicevox_core.dylib"));
-			RuntimeDependencies.Add("$(ProjectDir)/Binaries/Mac/libonnxruntime.1.13.1.dylib", Path.Combine(ModuleDirectory, "osx", "libonnxruntime.1.13.1.dylib"));
+			RuntimeDependencies.Add($"$(ProjectDir)/Binaries/{binPlatformName}/libvoicevox_core.dylib", Path.Combine(ModuleDirectory, platformName, "libvoicevox_core.dylib"));
+			RuntimeDependencies.Add($"$(ProjectDir)/Binaries/{binPlatformName}/libonnxruntime.1.13.1.dylib", Path.Combine(ModuleDirectory, platformName, "libonnxruntime.1.13.1.dylib"));
 			
 			// Open JTalkライブラリフォルダもコピーする
-			DeleteDirectory($"{ProjectDirectory}/Binaries/Mac/{OpenJtalkDicName}");
-			DirectoryCopy(Path.Combine(ModuleDirectory, "osx", OpenJtalkDicName), $"{ProjectDirectory}/Binaries/Mac/{OpenJtalkDicName}", true);
-			
+			AddRuntimeDependenciesDirectory(OpenJtalkDicName, platformName, binPlatformName, true);
 			// modelフォルダもコピーする
-			DeleteDirectory($"{ProjectDirectory}/Binaries/Mac/model");
-			DirectoryCopy(Path.Combine(ModuleDirectory, "osx", "model"), $"{ProjectDirectory}/Binaries/Mac/model", true);
+			AddRuntimeDependenciesDirectory("model", platformName, binPlatformName, true);
 		}
 		
 		PublicDefinitions.Add($"OPEN_JTALK_DIC_NAME=\"{OpenJtalkDicName}\"");
@@ -105,20 +104,20 @@ public class VoicevoxCore : ModuleRules
 	{
 		get
 		{
-			foreach (var commandLineArg in Environment.GetCommandLineArgs())
+			foreach (var CommandLineArg in Environment.GetCommandLineArgs())
 			{
-				var nameAndValues = new List<string>(commandLineArg.Split('=', StringSplitOptions.RemoveEmptyEntries));
-				if (nameAndValues.Count < 2)
+				var NameAndValues = new List<string>(CommandLineArg.Split('=', StringSplitOptions.RemoveEmptyEntries));
+				if (NameAndValues.Count < 2)
 				{
 					continue;
 				}
-				if (nameAndValues[0] != "-Project")
+				if (NameAndValues[0] != "-Project")
 				{
 					continue;
 				}
-				nameAndValues.RemoveAt(0);
-				var uProjectFilePath = string.Join("", nameAndValues.ToArray());
-				return Path.GetDirectoryName(uProjectFilePath);
+				NameAndValues.RemoveAt(0);
+				var UProjectFilePath = string.Join("", NameAndValues.ToArray());
+				return Path.GetDirectoryName(UProjectFilePath);
 			}
 
 			return null;
@@ -126,71 +125,35 @@ public class VoicevoxCore : ModuleRules
 	}
 
 	/// <summary>
-	/// 指定したディレクトリのコピー実行
+	/// 指定したディレクトリをRuntimeDependenciesに登録
 	/// </summary>
-	/// <param name="SourceDirName">コピー元のディレクトリ</param>
-	/// <param name="DestDirName">コピー先のディレクトリ</param>
+	/// <param name="SourceDirName">コピー元のディレクトリ名</param>
+	/// <param name="Platform">プラグイン側のプラットフォームフォルダ名</param>
+	/// <param name="BinPlatform">出力バイナリのプラットフォームフォルダ名</param>
 	/// <param name="CopySubDirs">サブディレクトリもコピーを行うか</param>
 	/// <exception cref="DirectoryNotFoundException"></exception>
-	private static void DirectoryCopy(string SourceDirName, string DestDirName, bool CopySubDirs)
+	private void AddRuntimeDependenciesDirectory(string SourceDirName, string Platform, string BinPlatform,  bool CopySubDirs)
 	{
-		Console.WriteLine(SourceDirName);
-		Console.WriteLine(DestDirName);
-		
-		var dir = new DirectoryInfo(SourceDirName);
-
-		if (!dir.Exists)
+		var Info = new DirectoryInfo(Path.Combine(ModuleDirectory, Platform, SourceDirName));
+		if (!Info.Exists)
 		{
 			throw new DirectoryNotFoundException($"The specified directory cannot be found: {SourceDirName}");
 		}
-
-		var dirs = dir.GetDirectories();
-		Directory.CreateDirectory(DestDirName);
 		
-		var files = dir.GetFiles();
-		foreach (var f in files)
+		var Infos = Info.GetDirectories();
+		var Files = Info.GetFiles();
+		
+		foreach (var F in Files)
 		{
-			var tempPath = Path.Combine(DestDirName, f.Name);
-			f.CopyTo(tempPath, true);
+			RuntimeDependencies.Add($"$(ProjectDir)/Binaries/{BinPlatform}/{SourceDirName}/{F.Name}", Path.Combine(ModuleDirectory, Platform, SourceDirName, F.Name));
 		}
 		
 		if (!CopySubDirs) return;
 		
-		foreach (var subDir in dirs)
+		foreach (var SubDir in Infos)
 		{
-			var tempPath = Path.Combine(DestDirName, subDir.Name);
-			DirectoryCopy(subDir.FullName, tempPath, true);
+			var TempPath = Path.Combine(SourceDirName, SubDir.Name);
+			AddRuntimeDependenciesDirectory(TempPath, Platform, BinPlatform, true);
 		}
 	}
-	
-	/// <summary>
-	/// 指定したディレクトリを削除
-	/// </summary>
-	/// <param name="TargetDirectoryPath">削除したいディレクトリ</param>
-	private static void DeleteDirectory(string TargetDirectoryPath)
-	{
-		if (!Directory.Exists(TargetDirectoryPath))
-		{
-			return;
-		}
-
-		//ディレクトリ以外の全ファイルを削除
-		var filePaths = Directory.GetFiles(TargetDirectoryPath);
-		foreach (var filePath in filePaths)
-		{
-			File.SetAttributes(filePath, FileAttributes.Normal);
-			File.Delete(filePath);
-		}
-
-		//ディレクトリの中のディレクトリも再帰的に削除
-		var directoryPaths = Directory.GetDirectories(TargetDirectoryPath);
-		foreach (var directoryPath in directoryPaths)
-		{
-			DeleteDirectory(directoryPath);
-		}
-
-		//中が空になったらディレクトリ自身も削除
-		Directory.Delete(TargetDirectoryPath, false);
-	}		
-	
 }
