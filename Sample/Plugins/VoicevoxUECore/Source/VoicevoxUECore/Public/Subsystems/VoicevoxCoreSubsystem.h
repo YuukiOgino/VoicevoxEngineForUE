a2890e6fdf330f8dede7f41e298a0ec7e9d6eeca
Yuki Ogino <r1050290.uv.tuc.ac.jp@gmail.com>
2024-09-14 09:26:37 +0900

    Subsystemを追加（中身は仮）

---
+++ Sample/Plugins/VoicevoxUECore/Source/VoicevoxUECore/Public/Subsystems/VoicevoxCoreSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "VoicevoxCoreSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOICEVOXUECORE_API UVoicevoxCoreSubsystem : public UEngineSubsystem
+{
+	GENERATED_BODY()
+	
+};
