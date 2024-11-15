a2fea2bd04f1f65db91cba08433d2e67d2617b19
YuukiOgino <r1050290.uv.tuc.ac.jp@gmail.com>
2024-11-15 15:31:26 +0900

    不要のCPPファイルを削除、読み方テキストを追加

--- Sample/Plugins/VoicevoxUECore/Source/VoicevoxUECore/Public/VoicevoxQuery.h
+++ Sample/Plugins/VoicevoxUECore/Source/VoicevoxUECore/Public/VoicevoxQuery.h
@@ -31,4 +31,15 @@ public:
 	//! 入力したテキスト
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="VOICEVOX CORE")
 	FString Text;
+
+	/**
+	 * @brief 読み方テキスト
+	 * @details 入力したテキストが正しい読み方をしない場合があるので、読み方がおかしい場合はYomikataTextに全角ひらがな、もしくは全角カタカナで手動で入力し、YomikataTextの値を元にAudioQueryを再度生成してください
+	 * @code
+	 *		UVoicevoxQuery*　assets;
+	 *		assets->VoicevoxAudioQuery = GEngine->GetEngineSubsystem<UVoicevoxCoreSubsystem>()->GetAudioQuery(assets->SpeakerType, assets->YomikataText, false)
+	 *　@endcode
+	 */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="VOICEVOX CORE")
+	FString YomikataText;
 };
