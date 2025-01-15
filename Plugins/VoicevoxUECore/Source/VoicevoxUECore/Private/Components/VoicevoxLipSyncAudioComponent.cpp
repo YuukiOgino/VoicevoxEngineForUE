c5bebc774a37884a91f4e9a9402684a0ae1bc9a3
YuukiOgino <r1050290.uv.tuc.ac.jp@gmail.com>
2025-01-15 21:30:46 +0900

    リップシンクのモーフターゲット名を格納するマップを追加。共通部分を関数化

--- Plugins/VoicevoxUECore/Source/VoicevoxUECore/Private/Components/VoicevoxLipSyncAudioComponent.cpp
+++ Plugins/VoicevoxUECore/Source/VoicevoxUECore/Private/Components/VoicevoxLipSyncAudioComponent.cpp
@@ -68,28 +68,26 @@ void UVoicevoxLipSyncAudioComponent::HandlePlaybackPercent(const UAudioComponent
 	{
 		const FVoicevoxLipSync LipSync = LipSyncList.Pop();
 		LipSyncTime += LipSync.Length;
+		FName MorphTargetName = NAME_None;
+		if (LipSync.VowelType != ELipSyncVowelType::Non && LipSync.VowelType != ELipSyncVowelType::CL)
+		{
+			MorphTargetName = LipSyncMorphNameMap[LipSync.VowelType];
+		}
 		if (OnLipSyncUpdate.IsBound())
 		{
-			OnLipSyncUpdate.Broadcast(LipSync);
+			OnLipSyncUpdate.Broadcast(LipSync, MorphTargetName);
 		}
 
 		if (OnLipSyncUpdateNative.IsBound())
 		{
-			OnLipSyncUpdateNative.Broadcast(LipSync);
+			OnLipSyncUpdateNative.Broadcast(LipSync, MorphTargetName);
 		}
 	}
 }
 
 void UVoicevoxLipSyncAudioComponent::PlayToText(const int SpeakerType, const FString Message, const bool bRunKana, const bool bEnableInterrogativeUpspeak, const float StartTime)
 {
-	if (bIsExecTts)
-	{
-		UE_LOG(LogVoicevoxLipSync, Warning, TEXT("合成音声生成中のため、音声再生をキャンセルしました。Delay等で少し時間を置いてから再度実行してください"));
-		const FColor Col = FColor::Yellow;
-		const FVector2D Scl = FVector2D(1.0f, 1.0f);
-		GEngine->AddOnScreenDebugMessage(-1, 3.0f, Col, TEXT("合成音声生成中のため、音声再生をキャンセルしました。Delay等で少し時間を置いてから再度実行してください"), true, Scl);
-		return;
-	}
+	if (CheckExecTts()) return;
 	
 	if (Sound != nullptr)
 	{
@@ -98,53 +96,14 @@ void UVoicevoxLipSyncAudioComponent::PlayToText(const int SpeakerType, const FSt
 	}
 	
 	PlayStartTime = StartTime;
-	TtsTask = UE::Tasks::Launch<>(TEXT("LipSyncComponentTextToSpeechTask"), [=]
-	{
-		bIsExecTts = true;
+	AudioQuery = GEngine->GetEngineSubsystem<UVoicevoxCoreSubsystem>()->GetAudioQuery(SpeakerType, Message, bRunKana);
 
-		// LipSyncに必要なデータを生成する
-		AudioQuery = GEngine->GetEngineSubsystem<UVoicevoxCoreSubsystem>()->GetAudioQuery(SpeakerType, Message, bRunKana);
-		LipSyncList = GEngine->GetEngineSubsystem<UVoicevoxCoreSubsystem>()->GetLipSyncList(AudioQuery);
-		Algo::Reverse(LipSyncList);
-		LipSyncTime = 0.0f;
-		// USoundWaveを生成する。Launch内でPlayを実行するとクラッシュするため、Play処理はTickTickComponentで行う
-		if (const TArray<uint8> OutputWAV = GEngine->GetEngineSubsystem<UVoicevoxCoreSubsystem>()->RunSynthesis(AudioQuery, SpeakerType, bEnableInterrogativeUpspeak);
-		!OutputWAV.IsEmpty())
-		{
-			FString ErrorMessage = "";
-			if (FWaveModInfo WaveInfo; WaveInfo.ReadWaveInfo(OutputWAV.GetData(), OutputWAV.Num(), &ErrorMessage))
-			{
-				USoundWaveProcedural* SoundWave = NewObject<USoundWaveProcedural>(USoundWaveProcedural::StaticClass());
-				const int32 ChannelCount = *WaveInfo.pChannels;
-				const int32 SizeOfSample = *WaveInfo.pBitsPerSample / 8;
-				const int32 NumSamples = WaveInfo.SampleDataSize / SizeOfSample;
-				const int32 NumFrames = NumSamples / ChannelCount;
-				
-				SoundWave->RawPCMDataSize = WaveInfo.SampleDataSize;
-				SoundWave->QueueAudio(WaveInfo.SampleDataStart, WaveInfo.SampleDataSize);
-				
-				SoundWave->Duration = static_cast<float>(NumFrames) / *WaveInfo.pSamplesPerSec;
-				SoundWave->SetSampleRate(*WaveInfo.pSamplesPerSec);
-				SoundWave->NumChannels = ChannelCount;
-				SoundWave->TotalSamples = *WaveInfo.pSamplesPerSec * SoundWave->Duration;
-				SoundWave->SoundGroup = SOUNDGROUP_Default;
-
-				SetSound(SoundWave);
-			}
-		}
-	});
+	ToSoundWave(SpeakerType, bEnableInterrogativeUpspeak);
 }
 
-void UVoicevoxLipSyncAudioComponent::PlayToAudioQuery(const FVoicevoxAudioQuery& Query, int64 SpeakerType, bool bEnableInterrogativeUpspeak, float StartTime)
+void UVoicevoxLipSyncAudioComponent::PlayToAudioQuery(const FVoicevoxAudioQuery& Query, const int64 SpeakerType, const bool bEnableInterrogativeUpspeak, const float StartTime)
 {
-	if (bIsExecTts)
-	{
-		UE_LOG(LogVoicevoxLipSync, Warning, TEXT("合成音声生成中のため、音声再生をキャンセルしました。Delay等で少し時間を置いてから再度実行してください"));
-		const FColor Col = FColor::Yellow;
-		const FVector2D Scl = FVector2D(1.0f, 1.0f);
-		GEngine->AddOnScreenDebugMessage(-1, 3.0f, Col, TEXT("合成音声生成中のため、音声再生をキャンセルしました。Delay等で少し時間を置いてから再度実行してください"), true, Scl);
-		return;
-	}
+	if (CheckExecTts()) return;
 	
 	if (Sound != nullptr)
 	{
@@ -154,43 +113,45 @@ void UVoicevoxLipSyncAudioComponent::PlayToAudioQuery(const FVoicevoxAudioQuery&
 	
 	AudioQuery = Query;
 	PlayStartTime = StartTime;
-	TtsTask = UE::Tasks::Launch<>(TEXT("LipSyncComponentTextToSpeechTask"), [=]
-	{
-		bIsExecTts = true;
-
-		// LipSyncに必要なデータを生成する
-		LipSyncList = GEngine->GetEngineSubsystem<UVoicevoxCoreSubsystem>()->GetLipSyncList(AudioQuery);
-		Algo::Reverse(LipSyncList);
-		LipSyncTime = 0.0f;
-		// USoundWaveを生成する。Launch内でPlayを実行するとクラッシュするため、Play処理はTickTickComponentで行う
-		if (const TArray<uint8> OutputWAV = GEngine->GetEngineSubsystem<UVoicevoxCoreSubsystem>()->RunSynthesis(AudioQuery, SpeakerType, bEnableInterrogativeUpspeak);
-		!OutputWAV.IsEmpty())
-		{
-			FString ErrorMessage = "";
-			if (FWaveModInfo WaveInfo; WaveInfo.ReadWaveInfo(OutputWAV.GetData(), OutputWAV.Num(), &ErrorMessage))
-			{
-				USoundWaveProcedural* SoundWave = NewObject<USoundWaveProcedural>(USoundWaveProcedural::StaticClass());
-				const int32 ChannelCount = *WaveInfo.pChannels;
-				const int32 SizeOfSample = *WaveInfo.pBitsPerSample / 8;
-				const int32 NumSamples = WaveInfo.SampleDataSize / SizeOfSample;
-				const int32 NumFrames = NumSamples / ChannelCount;
-				
-				SoundWave->RawPCMDataSize = WaveInfo.SampleDataSize;
-				SoundWave->QueueAudio(WaveInfo.SampleDataStart, WaveInfo.SampleDataSize);
-				
-				SoundWave->Duration = static_cast<float>(NumFrames) / *WaveInfo.pSamplesPerSec;
-				SoundWave->SetSampleRate(*WaveInfo.pSamplesPerSec);
-				SoundWave->NumChannels = ChannelCount;
-				SoundWave->TotalSamples = *WaveInfo.pSamplesPerSec * SoundWave->Duration;
-				SoundWave->SoundGroup = SOUNDGROUP_Default;
-
-				SetSound(SoundWave);
-			}
-		}
-	});
+	ToSoundWave(SpeakerType, bEnableInterrogativeUpspeak);
 }
 
-void UVoicevoxLipSyncAudioComponent::ToSoundWave(int64 SpeakerType, bool bEnableInterrogativeUpspeak)
+void UVoicevoxLipSyncAudioComponent::PlayToAudioQueryAsset(UVoicevoxQuery* VoicevoxQuery, const bool bEnableInterrogativeUpspeak, const float StartTime)
+{
+	if (CheckExecTts()) return;
+	
+	if (Sound != nullptr)
+	{
+		Stop();
+		SetSound(nullptr);
+	}
+
+	PlayStartTime = StartTime;
+	AudioQuery = VoicevoxQuery->VoicevoxAudioQuery;
+	ToSoundWave(VoicevoxQuery->SpeakerType, bEnableInterrogativeUpspeak);
+}
+
+/**
+ * テキストから音声変換を実行中かチェック
+ */
+bool UVoicevoxLipSyncAudioComponent::CheckExecTts() const
+{
+	if (bIsExecTts)
+	{
+		UE_LOG(LogVoicevoxLipSync, Warning, TEXT("合成音声生成中のため、音声再生をキャンセルしました。Delay等で少し時間を置いてから再度実行してください"));
+		const FColor Col = FColor::Yellow;
+		const FVector2D Scl = FVector2D(1.0f, 1.0f);
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, Col, TEXT("合成音声生成中のため、音声再生をキャンセルしました。Delay等で少し時間を置いてから再度実行してください"), true, Scl);
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * @brief AudioQueryからSoundWaveへ変換
+ */
+void UVoicevoxLipSyncAudioComponent::ToSoundWave(const int64 SpeakerType, const bool bEnableInterrogativeUpspeak)
 {
 	TtsTask = UE::Tasks::Launch<>(TEXT("LipSyncComponentTextToSpeechTask"), [=]
 	{
--- Plugins/VoicevoxUECore/Source/VoicevoxUECore/Public/Components/VoicevoxLipSyncAudioComponent.h
+++ Plugins/VoicevoxUECore/Source/VoicevoxUECore/Public/Components/VoicevoxLipSyncAudioComponent.h
@@ -9,14 +9,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "VoicevoxQuery.h"
 #include "VoicevoxUEDefined.h"
 #include "Components/AudioComponent.h"
 #include "Tasks/Task.h"
 #include "VoicevoxLipSyncAudioComponent.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLipSyncUpdate, FVoicevoxLipSync, VoicevoxLipSync);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnLipSyncUpdate, FVoicevoxLipSync, VoicevoxLipSync, FName, MorphTargetName);
 
-DECLARE_MULTICAST_DELEGATE_OneParam(FOnLipSyncUpdateNative, FVoicevoxLipSync);
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnLipSyncUpdateNative, FVoicevoxLipSync, FName);
 
 /**
  * @class UVoicevoxLipSyncAudioComponent
@@ -55,7 +56,18 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	/**
+	 * @brief AudioQueryからSoundWaveへ変換
+	 * @param [in] SpeakerType 
+	 * @param [in] bEnableInterrogativeUpspeak 
+	 */
 	void ToSoundWave(int64 SpeakerType, bool bEnableInterrogativeUpspeak = true);
+
+	/**
+	 * テキストから音声変換を実行中かチェック
+	 * @return trueの場合はテキストから音声変換のタスク実行中
+	 */
+	bool CheckExecTts() const;
 	
 public:
 
@@ -63,6 +75,17 @@ public:
 	FOnLipSyncUpdate OnLipSyncUpdate;
 	
 	FOnLipSyncUpdateNative OnLipSyncUpdateNative;
+
+	//! リップシンクで使用するモーフターゲット名のマップ
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, meta=(AllowPrivateAccess=true))
+	TMap<ELipSyncVowelType, FName> LipSyncMorphNameMap =
+		{
+			{ELipSyncVowelType::A, NAME_None},
+			{ELipSyncVowelType::I, NAME_None},
+			{ELipSyncVowelType::U, NAME_None},
+			{ELipSyncVowelType::E, NAME_None},
+			{ELipSyncVowelType::O, NAME_None},
+		};
 	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
@@ -73,6 +96,9 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	void PlayToAudioQuery(const FVoicevoxAudioQuery& Query, int64 SpeakerType, bool bEnableInterrogativeUpspeak = true, float StartTime = 0.0f);
+
+	UFUNCTION(BlueprintCallable)
+	void PlayToAudioQueryAsset(UVoicevoxQuery* VoicevoxQuery, bool bEnableInterrogativeUpspeak = true, float StartTime = 0.0f);
 };
 
 DECLARE_LOG_CATEGORY_EXTERN(LogVoicevoxLipSync, Log, All);
